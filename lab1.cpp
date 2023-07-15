ddba4b1457919d2cb15a5c076e70e3105802bc1c
keval <kevalkeval545@gmail.com>
2023-07-14 19:05:17 -1100

    randomized_quicksort

---
+++ lab1.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+int count1 = 0;
+
+int partition(int arr[], int p, int q){
+    int z = p-1;
+    int pivot = arr[q];
+    for(int j = p; j < q; j++){
+        if(arr[j]<pivot){
+            z++;
+            swap(arr[z],arr[j]);
+        }
+    }
+    count1 = count1 + (q-p);
+    swap(arr[z+1],arr[q]);
+    return z+1;
+}
+
+int randomize_partition(int arr[],int p, int q){
+    int t = p + rand()%(q-p+1);
+    swap(arr[t],arr[q]);
+    return(partition(arr,p,q));
+}
+
+void swap(int &a, int &b){
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void randomize_quickSort(int arr[], int p, int q){
+    
+    if(p < q){
+        int mid = randomize_partition(arr,p,q);
+        randomize_quickSort(arr,p,mid-1);
+        randomize_quickSort(arr,mid+1,q);
+    }
+}
+
+int main()
+{
+    srand(time(NULL));
+    int arr[1000];
+    for(int i = 0; i < 1000; i++){
+        arr[i] = 1000-i;
+    }
+    
+    randomize_quickSort(arr,0,999);
+    
+    cout<<count1<<" ";
+
+    return 0;
+}
+
+
