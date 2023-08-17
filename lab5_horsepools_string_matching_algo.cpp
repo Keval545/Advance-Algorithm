2290cd69c339eff9c02e817bfbcd7aa952222466
keval <kevalkeval545@gmail.com>
2023-08-17 20:34:38 +0530

    lab5

---
+++ lab5_horsepools_string_matching_algo.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    string text;
+    cout << "Enter your text : ";
+    getline(cin, text, '\n');
+    
+    string pattern;
+    cout << "Enter your pattern to find in text : ";
+    getline(cin, pattern, '\n');
+    
+    int n = text.size();
+    int m = pattern.size();
+    
+    map<char, int> shift_table;
+    bool foo = false;
+    
+    // Creating Shift Table
+    int k = m-1;
+    for(int i=0 ; i<m-1 ; i++) {
+        shift_table[pattern[i]] = k;
+        k--;
+    }
+    
+    for(int i=0 ; i<=n-m ; ) {
+        int p = i+m-1;
+        int q = m-1;
+        
+        while(q >= 0 && p >= 0 && pattern[q] == text[p]) {
+            p--;
+            q--;
+        }
+        
+        if(q == -1) {
+            cout << "Pattern matched at : " << i << endl;
+            i++;
+            foo = true;
+        }
+        else {
+            if(shift_table[text[i+m-1]] > 0) {
+                i += shift_table[text[i+m-1]];
+            }
+            else {
+                i += m;
+            }
+        }
+    }
+    
+    if(foo == false) {
+        cout << "No match is present" << endl;
+    }
+    
+    cout << "computation is done" << endl;
+    
+    return 0;
+}
