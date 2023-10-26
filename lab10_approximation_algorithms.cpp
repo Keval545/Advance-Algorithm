0044a7f0219eb71a477f7cd55c76c232b14246e4
Keval545 <108585269+Keval545@users.noreply.github.com>
2023-10-27 00:21:39 +0530

    Update lab10_approximation_algorithms.cpp

--- lab10_approximation_algorithms.cpp
+++ lab10_approximation_algorithms.cpp
@@ -1,57 +1,71 @@
+//Randomized approximation of MVC.
+//Time complexity (average) : O(E), E = no. of Edges. POLYNOMIAL.
+
 #include<bits/stdc++.h>
 using namespace std;
 
-class Graph
-{
-	int V; 
-	list<int> *adj; 
-public:
-	Graph(int V){
-	    this->V = V;
-    	adj = new list<int>[V];
-	}
-	void addEdge(int v, int w){
-	    adj[v].push_back(w); 
-    	adj[w].push_back(v); 
-	}
-	void printVertexCover(){
-	    bool visited[V];
-	for (int i=0; i<V; i++){
-		visited[i] = false;
-  }
+// To perform the approx. algorithm for Vertex Cover using Sets.
+
+set<int> approximateVertexCover(vector<pair<int, int>> edges) {
+    srand(time(NULL));
     
-	list<int>::iterator i;
+    set<int> result;
+    //converting vector of edges into set.
+    set<pair<int, int>> E(edges.begin(), edges.end());
+    
+    //Loop till all edges are not removed (Basically, till all vertices are not covered).
+    while (!E.empty()) {
+        // Pick an edge (u, v) from set E
+        int randomIndex = rand() % E.size();
 
-	for (int u=0; u<V; u++)
-	{
-		if (visited[u] == false)
-		{   
-			for (i= adj[u].begin(); i != adj[u].end(); ++i)
-			{
-				int v = *i;
-				if (visited[v] == false)
-				{
-					visited[v] = true;
-					visited[u] = true;
-					break;
-				}
-			}
-		}
-	}
-	
-	for (int i=0; i<V; i++)
-		if (visited[i])
-		cout << i << " ";
+        // Advance the iterator to the random index
+        auto it = E.begin();
+        advance(it, randomIndex);
+        
+        int u = it->first;
+        int v = it->second;
+
+        // Add 'u' and 'v' to the result
+        result.insert(u);
+        result.insert(v);
+
+        // Remove all edges from E which are either incident on u or v
+        vector<pair<int, int>> toRemove;
+        for (const auto& edge : E) {
+            if (edge.first == u || edge.first == v || edge.second == u || edge.second == v) {
+                toRemove.push_back(edge);
+            }
+        }
+        
+        //Erase those edges which are present in toRemove vec from original edge vector.
+        for (const auto& edgeToRemove : toRemove) {
+            E.erase(edgeToRemove);
+        }
+    }
+
+    return result;
 }
-};
 
-int main()
-{
-	Graph g(7);
-	g.addEdge(0, 1);
-	g.addEdge(3, 2);
-	g.addEdge(4, 5);
+int main() {
+    int V, E;
+    cout << "Enter no. of vertices and Edges in a graph :\n";
+    cin >> V >> E;
+    
+    vector<pair<int, int>> edges(E);
+    
+    int u, v;
+    for(int i=0; i<E; i++) {
+        cout << "Enter endpoints of edge " << i+1 << " in a graph :\n";
+        cin >> u >> v;
+        
+        edges[i] = {u, v};
+    }
+    
+    set<int> VC = approximateVertexCover(edges);
+    
+    cout << "Approximate Vertex cover :\n";
+    for(auto v: VC) cout << v << " ";
   
-	g.printVertexCover();
-	return 0;
+    
+    return 0;
 }
